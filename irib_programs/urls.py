f7d12660b37f36dbb18b40e383d48550f1fd1100
Meisam <meisam1984@users.noreply.github.com>
2025-12-04 09:12:36 -1200

    fix(urls): add root redirect to admin

--- irib_programs/urls.py
+++ irib_programs/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="admin/", permanent=False)),
     path("admin/", admin.site.urls),
     path("api/", include("programs.api.urls")),
     path("channels/<int:channel_id>/schedule/<str:date_str>/", include("programs.urls.timeline_urls")),
